57de87885bd8e172be4f3ca010884260c2f2729c
Sanchitraina1999 <sanchitraina1999@gmail.com>
2019-08-07 16:04:46 +0530

    questions added successfully

--- userApp/models.py
+++ userApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class QUESTION(models.Model):
     title_number = models.CharField(max_length=50)
-    question = models.CharField(max_length=500)
+    question = models.CharField(max_length=5000)
 
     def __str__(self):
             return self.title_number + '-' + self.question
