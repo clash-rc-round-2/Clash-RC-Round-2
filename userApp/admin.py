a1188c103389fe4ff42bc10f4963261ccbcd79d2
gauravghati <gauravghati225@gmail.com>
2019-08-11 13:39:26 +0530

    Bug Fixed!

--- userApp/admin.py
+++ userApp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile, Question, Submission
+from .models import User, Question, Submission
 
 admin.site.register(Question)
-admin.site.register(UserProfile)
+admin.site.register(User)
 admin.site.register(Submission)
--- userApp/models.py
+++ userApp/models.py
@@ -1,9 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
-    username = models.CharField(max_length=250)
-    password = models.CharField(max_length=250)
+class User(models.Model):
     score = models.IntegerField(default=0)
 
     def __str__(self):
@@ -13,7 +12,6 @@ class UserProfile(models.Model):
 class Question(models.Model):
     title_number = models.CharField(max_length=50)
     question = models.CharField(max_length=5000)
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
             return self.title_number + '-' + self.question
@@ -21,6 +19,6 @@ class Question(models.Model):
 
 class Submission(models.Model):
     time = models.DateTimeField()
-    input = models.CharField(max_length=1000)
+    code = models.CharField(max_length=1000)
     output = models.CharField(max_length=100)
     que = models.ForeignKey(Question, on_delete=models.CASCADE)
