eff3bdcec272a903035e431f0998dab8f836c4c5
gauravghati <gauravghati225@gmail.com>
2019-08-17 19:38:33 +0530

    Login Page Integrated!

---
+++ data/usersCode/user123321/question1/code1.0.cpp
@@ -0,0 +1,2 @@
+
+soultion 1
--- userApp/views.py
+++ userApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login
 from django.contrib.auth.models import User
-from .models import Question, Submission
+from .models import Question, Submission, UserProfile
 import os
 
 cwd = os.getcwd()
@@ -11,14 +11,22 @@ def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        name1 = request.POST.get('name1')
+        name2 = request.POST.get('name2')
+        phone1 = request.POST.get('phone1')
+        phone2 = request.POST.get('phone2')
+        email1 = request.POST.get('email1')
+        email2 = request.POST.get('email2')
         user = User.objects.create_user(username=username, password=password)
+        userprofile = UserProfile(user=user, name1=name1, name2=name2, phone1=phone1, phone2=phone2, email1=email1,
+                                  email2=email2)
         os.chdir('%s/data/usersCode' % cwd)
         os.mkdir(username)
         login(request, user)
         return redirect(reverse("detail"))
 
     elif request.method == 'GET':
-        return render(request, 'userApp/signup.html')
+        return render(request, 'userApp/clashlogin.html')
 
 
 def detail(request):
@@ -35,10 +43,12 @@ def file(request, username, qn):
         submission = Submission(code=content, user=user, que=question)
         submission.save()
         os.chdir(f'{cwd}/data/usersCode/{username}')
+
         try:
             os.mkdir(f'question{qn}')
         except(FileExistsError):
             pass
+
         os.chdir(f'question{qn}/')
         codefile = open(f"code{qn}.{att}.cpp", "w+")
         codefile.write(content)
