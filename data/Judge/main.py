eb59731b940859d7f29d220bdd54c6e57077360c
gauravghati <gauravghati225@gmail.com>
2019-08-29 23:55:06 +0530

    Sandbox Structure!

--- data/Judge/main.py
+++ data/Judge/main.py
@@ -26,17 +26,32 @@ def configSandbox(args, inputFile, outFile):
     cookbook = {
         'args': args[1:],               # targeted program
         'stdin': inputFile,             # input to targeted program
-        'stdout': outFile,           # output from targeted program
+        'stdout': outFile,              # output from targeted program
         'stderr': sys.stderr,           # error from targeted program
         'quota': dict(wallclock=30000,  # 30 sec
                       cpu=2000,         # 2 sec
                       memory=8388608,   # 8 MB
                       disk=1048576)}    # 1 MB
     # create a sandbox instance and execute till end
-    msb = sandbox(**cookbook)
+    msb = sandbox.Sandbox(**cookbook)
     msb.run()
     # verbose statistics
     sys.stderr.write("result: %(result)s\ncpu: %(cpu)dms\nmem: %(mem)dkB\n" %
         msb.probe())
     return os.EX_OK
 
+def compare():
+    #comparing.
+
+def compile():
+    #compilation
+
+def runTestCases():
+    #runTestCases
+
+def main():
+    # Configuration of Sandbox
+    # calling configSandbox()
+    # calling compile()
+    # calling runTestCases()
+    # calling compare()
