8869667cec1dc163a972a21ea62e9b746c599e08
gauravghati <gauravghati225@gmail.com>
2019-08-31 18:22:32 +0530

    Run test Cases fun.

--- data/Judge/main.py
+++ data/Judge/main.py
@@ -1,5 +1,5 @@
 import os
-import sandbox
+from sandbox import *
 import sys
 
 NO_OF_QUESTIONS = 6
@@ -7,8 +7,8 @@ NO_OF_QUESTIONS = 6
 # paths
 path = os.getcwd()    # 'clash'
 path_userCode = path + '/data/usersCode'
-input_path = path +  '/data/standard/input'
-output_path = path +  '/data/standard/output'
+input_path = path + '/data/standard/input'
+output_path = path + '/data/standard/output'
 
 
 try:
@@ -50,27 +50,38 @@ def config_sandbox(args, input_file, out_file):
     return os.EX_OK
 
 
+def probe(self):
+    # add custom entries into the probe dict
+    d = sandbox.probe(self, False)
+    d['cpu'] = d['cpu_info'][0]
+    d['mem'] = d['mem_info'][1]
+    d['result'] = self.result
+    return d
+
+
 def compile(filename, username, extension, que_id):
     return_value = 1         # By default error while running file.
     if extension == 'c':
         return_value = os.system('gcc ' + '-o ' + '{}/{}/solution{} '.format(path_userCode, username, que_id) +
-                                 filename)
+                                 path_userCode + '/{}/question{}/{}.c'.format(username, que_id, filename))
 
     elif extension == 'cpp':
         return_value = os.system('g++ ' + '-o ' + '{}/{}/solution{} '.format(path_userCode, username, que_id) +
-                                 filename)
+                                 path_userCode + '/{}/question{}/{}.cpp'.format(username, que_id, filename))
 
     return return_value      # return 0 for success and 1 for
 
 
-def run_test_cases(filename, username, que_id, attempts):
+def run_test_cases(test_case_no, filename, extension, username, que_id, attempts):
+    input_file = input_path + '/input{}.txt'.format(que_id)
+    input_f = open("input_file", "r")
+    code_file = path_userCode + '/{}/question{}/{}.{}'.format(username, que_id, filename, extension)
+    code_f = open('output_file', os.O_RDWR)
+
+    output_of_code = os.system('./{}/{}/solution{}'.format(path_userCode, username, que_id))
 
 
 def main():
-    # main.py is called from views.py by command
-    # os.system('python main.py '+ '{}/{}/question{}/code{}-{}.cpp'.format(path1, username, qn, qn, attempts) + ' '+
-    #          username + ' ' + qn + ' ' + attempts)
-
     filename = sys.argv[1]                    # FileName
     extension = sys.argv[1].split(".")        # C or CPP
     username = sys.argv[2]                    # Username
@@ -79,6 +90,6 @@ def main():
 
     return_value = compile(filename, username, extension, que_id)          # calling compile()
 
-    if return_value==0:
+    if return_value == 0:
         for i in range(0, NO_OF_QUESTIONS-1):
-            run_test_cases(filename, username, que_id, attempts)           # calling runTestCases()
+            run_test_cases(i, filename, username, extension, que_id, attempts)           # calling runTestCases()
