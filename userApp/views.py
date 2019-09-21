0e2ff357cc42c635ed00fe63085e74e6c40e89b5
kulkarnisaumitra98 <kulkarnisaumitra98@gmail.com>
2019-09-21 19:05:45 +0530

    remove sandbox headers and imports

--- userApp/views.py
+++ userApp/views.py
@@ -214,7 +214,12 @@ def codeSave(request, username, qn):
                 attempts=att,
                 lang=extension
             )
+
             print(type(testcase_values))
+
+            with open(code_file, 'w+') as f:
+                f.write(content)
+                f.close()
 
             now_time = datetime.datetime.now()
             now_time_sec = now_time.second + now_time.minute * 60 + now_time.hour * 60 * 60
@@ -262,7 +267,6 @@ def codeSave(request, username, qn):
 
             var = calculate()
 
-            var = calculate()
             data = {
                 'testcase': testcase_values,
                 'error': error_text,
